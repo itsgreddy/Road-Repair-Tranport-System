f376836f4d3fd4a2f847d2926c6e5964151c5b7e
Harsha <81374435+Harshavardhan7678@users.noreply.github.com>
2023-04-01 12:28:57 +0300

    Navbar CSS

--- rrts/src/components/Navbar.jsx
+++ rrts/src/components/Navbar.jsx
@@ -14,15 +14,28 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex justify-between bg-gray-200 w-full p-4">
-      <h1 className="text-center text-2xl font-bold">
-      <Link to="/">Road Repair & Transport System</Link>
-      </h1>
-      {/* {user?.displayName ? (
+    <div className="bg-gray-800 text-white">
+      <div className="flex justify-between w-full p-4">
+        <h1 className="text-center text-2xl font-bold">
+          <Link to="/">Road Repair & Transport System</Link>
+        </h1>
+       <div className="flex gap-3 ">
+        <Link to="/clerk">
+          <button className="p-2" >Clerk</button>
+        </Link>
+        <Link to="/officer">
+          <button className="p-2" >Community Officer</button>
+        </Link>
+        <Link to="/supervisor">
+          <button className="p-2" >Supervisor</button>
+        </Link>
+      </div>        
+        {/* {user?.displayName ? (
         <button onClick={handleSignOut}>Logout</button>
       ) : (
         <Link to="/clerksignin">Sign in</Link>
       )} */}
+      </div>
     </div>
   );
 };
